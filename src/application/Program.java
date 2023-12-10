c9deffd0cefa42fb591294f8d3bfd929cebbd235
Henrique Ferreira <henriqueferreiraferro@gmail.com>
2023-12-10 18:21:17 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,27 @@
 package application;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		
 		Department obj = new Department(1,"Books");
 		System.out.println(obj);
+		
+		Seller seller = new Seller
+			(10,"Peter Brown", 
+			"pete@gmail.com",
+			new Date(),2000.0, obj);
+	
+		System.out.println(seller);
+		
 	}
 
 }
